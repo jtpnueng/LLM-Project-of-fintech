5e589408bc48c05370ed68328275fa06a6e6ebee
jtpnueng <nueng_jatu_2543@hotmail.com>
2026-03-05 08:05:34 +0700

    Change axes
    Change for built axes

--- first_python.py
+++ first_python.py
@@ -2,5 +2,22 @@ print ("hello world")
 
 X =1 
 Y =2
-f
+
 import matplotlib as plt
+import pandas as pd
+import numpy as np
+
+url = ('www.example.com')
+
+df = pd.read(url)
+
+df.sample(5)
+
+df.dedcribe
+
+df = df.drop('Enginsize', 'CO2', 'credit')
+
+df.corr
+
+axes = pd.ploting_matrix(df, alpha=0.2)
+
